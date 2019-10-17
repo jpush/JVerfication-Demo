761322f1514d00efbd5dbac77cf12e88cb725d28
ios开发-侯彦辉 <houyh@jiguang.cn>
2019-10-17 10:31:25 +0800

    QQ/Wechat/新浪微博授权登录功能实现
    集成JShareSDK 实现授权登录功能。

--- ios-jverification-demo/demo/demo/AppDelegate.m
+++ ios-jverification-demo/demo/demo/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "JVERIFICATIONService.h"
+#import "JSHAREService.h"
 
 @interface AppDelegate ()
 
@@ -20,13 +21,32 @@
     // Override point for customization after application launch.
     //集成极光认证sdk
     JVAuthConfig *config = [[JVAuthConfig alloc] init];
-    config.appKey = @"填入在极光官网申请的AppKey";
+//    config.appKey = @"填入在极光官网申请的AppKey";
+    config.appKey = @"a0e6ace8d5b3e0247e3f58db";
+
+    
     [JVERIFICATIONService setupWithConfig:config];
     [JVERIFICATIONService setDebug:YES];
+    [self shareSDKConfig];
     return YES;
 }
 
+- (void)shareSDKConfig{
+    JSHARELaunchConfig *config = [[JSHARELaunchConfig alloc] init];
+    config.appKey = @"a0e6ace8d5b3e0247e3f58db";
+    config.WeChatAppId = @"wx4a58c62d258121ac";
+    config.WeChatAppSecret = @"6f43e3fca5b2c3996c20faf5c2a08729";
 
+    config.SinaWeiboAppKey = @"2906641376";
+    config.SinaWeiboAppSecret = @"b495eedd2ac836895eb06c971e521073";
+    
+    config.SinaRedirectUri = @"https://www.jiguang.cn";
+    config.QQAppId = @"101789350";
+    config.QQAppKey = @"8bd761ec8be03a0c75477ad1d4eb2a03";
+    config.isSupportWebSina = YES;
+    [JSHAREService setupWithConfig:config];
+    [JSHAREService setDebug:YES];
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -51,6 +71,11 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url{
+    [JSHAREService handleOpenUrl:url];
+    return YES;
 }
 
 @end
